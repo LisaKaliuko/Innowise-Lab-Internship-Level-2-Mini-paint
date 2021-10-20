0767e9c70f7e2091c8c577c1b5c014592707b876
Lizaveta Kaliuka <lisakaliuko@gmail.com>
2021-10-20 09:38:54 +0300

    add firebase

---
+++ src/core/firebase/index.ts
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+import { firebaseConfig } from '../../config/fb.config';
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+
+export const firestore = firebase.firestore();
+
+export default firebase;
