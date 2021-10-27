bcbdc04f6d0119f6f45076a44d15cc68ef311843
Lizaveta Kaliuka <lisakaliuko@gmail.com>
2021-10-27 14:49:51 +0300

    add function to gallery

--- src/pages/Paint/components/buttons-panel/buttons-panel.component.tsx
+++ src/pages/Paint/components/buttons-panel/buttons-panel.component.tsx
@@ -1,6 +1,8 @@
 import React, { FC } from 'react';
+import { useHistory } from 'react-router';
 
 import { CanvasSize } from '../../../../shared/constants/canvas-size.constants';
+import { AppRouteNames } from '../../../../shared/constants/app-route-names.constants';
 import { Container, Button } from './buttons-panel.styles';
 
 interface ButtonsPanelProps {
@@ -10,12 +12,14 @@ interface ButtonsPanelProps {
 const ButtonsPanelComponent: FC<ButtonsPanelProps> = ({
   context,
 }): JSX.Element => {
+  const history = useHistory();
+
   const clearSheet = () => {
     if (context) context.clearRect(0, 0, CanvasSize.width, CanvasSize.height);
   };
 
   const savePicture = () => console.log('save picture');
-  const toGallery = () => console.log('go to gallery');
+  const toGallery = () => history.push(AppRouteNames.Gallery);
 
   return (
     <Container>
